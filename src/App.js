24a6c06571fdafb446b4025ec45883c554a96c59
SuhasChinnam <129885060+SuhasChinnam@users.noreply.github.com>
2023-10-22 09:52:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import ResponsiveParagraphWordCounter from "./ResponsiveParagraphWordCounter";
+
+function App() {
+  return (
+    <div>
+      <ResponsiveParagraphWordCounter />
+    </div>
+  );
+}
+
+export default App;
